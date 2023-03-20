513c7efe5eebedd2537005ea47fd8caf52f38211
NeetaMourya-BL <neeta.mourya@bridgelabz.com>
2023-03-20 17:16:36 +0530

    Add: .gitignore and README.md are files are added

---
+++ src/com/bridgelabz/employeewagecomputationprogram/EmployeeWageComputationProgram.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeewagecomputationprogram;
+
+public class EmployeeWageComputationProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+
+}
